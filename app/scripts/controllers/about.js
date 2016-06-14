4b7e47f7d9665491fa6627024252547a9cd71e28
Weston Pace <Weston.Pace@viavisolutions.com>
2016-06-13 23:17:43 -0600

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wpPcardsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wpPcardsApp
+ */
+angular.module('wpPcardsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wpPcardsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wpPcardsApp
+ */
+angular.module('wpPcardsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
